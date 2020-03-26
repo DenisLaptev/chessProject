d6efb87bffa77e51621756d1b3ecf86e6ebba4d8
Denis <laptev.denis.18@gmail.com>
2020-03-26 19:36:57 +0200

    Refactoring. 26-03-2020

--- src/com/nx/chessproject/Main.java
+++ src/com/nx/chessproject/Main.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static final int SQUARE_SIDE = 3;
+
     public static final char BLACK_SQUARE_SIMBOL = 'X';
     public static final char WHITE_SQUARE_SIMBOL = ' ';
 
@@ -14,39 +15,61 @@ public class Main {
     public static final String ENTER_HIGHT_MESSAGE = "Enter Height:";
 
     public static int numberConsoleInput(String message) throws IOException {
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println(message);
+
         int number = Integer.parseInt(bufferedReader.readLine());
+
         return number;
     }
 
-    public static char definePixelChar(int xIntDiv, int yIntDiv) {
+    public static char getPixelChar(int xPos, int yPos, int squareSide) {
+
+        char pixelChar = BLACK_SQUARE_SIMBOL;
+
+        int xIntDiv = xPos / squareSide;
+        int yIntDiv = yPos / squareSide;
 
         if (((xIntDiv % 2 == 0) && (yIntDiv % 2 == 0))
                 || ((xIntDiv % 2 != 0) && (yIntDiv % 2 != 0))) {
-            return WHITE_SQUARE_SIMBOL;
-        } else {
-            return BLACK_SQUARE_SIMBOL;
+            pixelChar = WHITE_SQUARE_SIMBOL;
         }
-
+        return pixelChar;
     }
 
-    public static void printChessDesk(int squareSide, int colsNumber, int rowsNumber) {
-        int xPixelsNumber = squareSide * colsNumber;
+    public static char[][] getChessDesk2DArray(int colsNumber, int rowsNumber, int squareSide) {
+
         int yPixelsNumber = squareSide * rowsNumber;
+        int xPixelsNumber = squareSide * colsNumber;
+
+        char[][] chessDesk2DArray = new char[yPixelsNumber][xPixelsNumber];
 
         for (int iy = 0; iy < yPixelsNumber; iy++) {
-            int yIntDiv = iy / squareSide;
+
             for (int ix = 0; ix < xPixelsNumber; ix++) {
-                int xIntDiv = ix / squareSide;
-                char pixelChar = definePixelChar(xIntDiv, yIntDiv);
-                System.out.print(pixelChar);
+
+                char pixelChar = getPixelChar(ix, iy, squareSide);
+                chessDesk2DArray[iy][ix] = pixelChar;
             }
-            System.out.print("\n");
+        }
+
+        return chessDesk2DArray;
+    }
+
+
+    public static void printChessDesk(char[][] chessDesk2DArray) {
+
+        for (char[] row : chessDesk2DArray) {
+            for (char element : row) {
+                System.out.print(element);
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
+
         int width = 0;
         int height = 0;
 
@@ -62,6 +85,8 @@ public class Main {
             e.printStackTrace();
         }
 
-        printChessDesk(SQUARE_SIDE, width, height);
+        char[][] chessDesk2DArray = getChessDesk2DArray(width, height, SQUARE_SIDE);
+
+        printChessDesk(chessDesk2DArray);
     }
 }
