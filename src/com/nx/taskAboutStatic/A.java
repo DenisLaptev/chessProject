4752a48255c6100bfe8103989026d8a4c1a4ce21
Denis <laptev.denis.18@gmail.com>
2020-03-27 18:03:59 +0200

    Android Bundle realization. How to set parameters without import static of constants.

--- src/com/nx/taskAboutStatic/A.java
+++ src/com/nx/taskAboutStatic/A.java
@@ -1,17 +1,12 @@
 package com.nx.taskAboutStatic;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import static com.nx.taskAboutStatic.B.USER_ID;
 
 public class A {
 
     public static void main(String[] args) {
 
-        Map<String,Long> params = new HashMap<>();
-
-        params.put(USER_ID, 1L);
+        Map<String,Long> params = B.createUserParameter(1L);
 
         new B().showUser(params);
     }
--- src/com/nx/taskAboutStatic/B.java
+++ src/com/nx/taskAboutStatic/B.java
@@ -5,9 +5,15 @@ import java.util.Map;
 
 public class B {
 
-    public static String USER_ID = "someParam";
+    private static String USER_ID = "userId";
 
-    public void showUser(Map<String,Long> params) {
+    public static Map<String, Long> createUserParameter(Long userId) {
+        Map<String, Long> param = new HashMap<>();
+        param.put(USER_ID, userId);
+        return param;
+    }
+
+    public void showUser(Map<String, Long> params) {
         System.out.println("Hello! params=" + params);
     }
 }
