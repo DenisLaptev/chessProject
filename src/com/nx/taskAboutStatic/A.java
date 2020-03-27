d9a23cf8f4a6315e4c11ab75eb050fbb1a17ac33
Denis <laptev.denis.18@gmail.com>
2020-03-27 16:27:45 +0200

    task about Static

---
+++ src/com/nx/taskAboutStatic/A.java
@@ -0,0 +1,8 @@
+package com.nx.taskAboutStatic;
+
+public class A {
+    public static void main(String[] args) {
+
+        new B().showUser(B.USER_ID, 1L);
+    }
+}
---
+++ src/com/nx/taskAboutStatic/B.java
@@ -0,0 +1,10 @@
+package com.nx.taskAboutStatic;
+
+public class B {
+
+    public static boolean USER_ID = true;
+
+    public void showUser(boolean userId, long longParam) {
+        System.out.println("Hello! userId="+userId+"longParam="+longParam);
+    }
+}
